40f09409a5923d1c7d376c1f8d807c04b08a0053
Semakos95 <geo.sem@hotmail.com>
2023-04-07 13:25:04 +0300

    gg

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -1,6 +1,10 @@
 package com.javatpoint.controller;
-
-public class HelloWorldController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/")
     public String hello()
     {
         return "Hello javaTpoint";
